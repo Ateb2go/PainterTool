e64ff6f55fa71cdce50d87abd56078a45572e503
Ahpla <rhghks1@naver.com>
2019-03-04 17:12:17 +0900

    commit dev 2

--- app/src/main/java/com/ateb2go/paintertool/DrawActivity.java
+++ app/src/main/java/com/ateb2go/paintertool/DrawActivity.java
@@ -1,75 +1,70 @@
 package com.ateb2go.paintertool;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.Matrix;
-import android.support.annotation.Nullable;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
-import android.view.SurfaceHolder;
-import android.view.ViewGroup;
+import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ZoomControls;
-
 
 public class DrawActivity extends AppCompatActivity {
 
+    CanvasView cv;
+
+    ImageView pen, paint, eraser, palette, spoid, undo, redo, save;
+
     RelativeLayout relativeLayout;
-
-    Context context=this;
-    DrawView drawView;
-
-    int width, height;
-    int sWidth, sHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_draw);
-        sWidth=getWindowManager().getDefaultDisplay().getWidth();
-        sHeight=getWindowManager().getDefaultDisplay().getHeight();
+        setContentView(R.layout.activity_draw);
+
+        relativeLayout=findViewById(R.id.relativeLayout);
+        pen=findViewById(R.id.pen);
+        paint=findViewById(R.id.paint);
+        eraser=findViewById(R.id.eraser);
+        palette=findViewById(R.id.palette);
+        spoid=findViewById(R.id.spoid);
+        undo=findViewById(R.id.undo);
+        redo=findViewById(R.id.redo);
+        save=findViewById(R.id.save);
 
         Intent intent=getIntent();
-        width=Integer.parseInt(intent.getStringExtra("width"));
-        height=Integer.parseInt(intent.getStringExtra("height"));
+        int width=Integer.parseInt(intent.getStringExtra("width"));
+        int height=Integer.parseInt(intent.getStringExtra("height"));
 
+        Rect rect=new Rect(0, 0, width, height);
 
-        relativeLayout=new RelativeLayout(this);
-
-        RelativeLayout.LayoutParams layoutParams=new RelativeLayout.LayoutParams(width*2+sWidth, height*2+sHeight);
-        relativeLayout.setBackgroundColor(getResources().getColor(R.color.darkgray));
-        relativeLayout.setLayoutParams(layoutParams);
-        setContentView(relativeLayout);//배경 생성
-
-
-        drawView=new DrawView(this);
-        drawView.setBackgroundColor(Color.WHITE);
-        SurfaceHolder surfaceHolder=drawView.getHolder();
-
-        if(width>sWidth||height>sHeight){
-            surfaceHolder.setFixedSize(sWidth, sHeight);
-            width=sWidth; height=sHeight;
-        }else surfaceHolder.setFixedSize(width, height);//임시로 width height가 화면크기를 넘기면 화면크기로 자동조정(줌 기능 만들기 전까지)
-        relativeLayout.addView(drawView);//그리는 view 배경에 붙임
-
-        RelativeLayout.LayoutParams params= (RelativeLayout.LayoutParams) drawView.getLayoutParams();
-        params.leftMargin=sWidth/2-width/2;
-        params.topMargin=sHeight/2-height/2;
-        drawView.setLayoutParams(params);//그리는 view 가운데로 옮기기
-
+        cv=new CanvasView(this, rect);
+        RelativeLayout.LayoutParams layoutParams=new RelativeLayout.LayoutParams(width, height);
+        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+        cv.setLayoutParams(layoutParams);
+        cv.setBackgroundColor(Color.WHITE);
+        relativeLayout.addView(cv);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        drawView.drawThread.stopThread();
+    public void clickIcons(View view) {
+        switch (view.getId()){
+            case R.id.pen:
+                cv.setPen();
+                eraser.setBackgroundColor(Color.TRANSPARENT);
+                pen.setBackgroundColor(Color.RED);
+                break;
+            case R.id.paint: Toast.makeText(this, "준비중입니다", Toast.LENGTH_SHORT).show(); break;
+            case R.id.eraser:
+                cv.setEraser();
+                pen.setBackgroundColor(Color.TRANSPARENT);
+                eraser.setBackgroundColor(Color.RED);
+                break;
+            case R.id.palette: Toast.makeText(this, "준비중입니다", Toast.LENGTH_SHORT).show();  break;
+            case R.id.spoid: Toast.makeText(this, "준비중입니다", Toast.LENGTH_SHORT).show();  break;
+            case R.id.undo: Toast.makeText(this, "준비중입니다", Toast.LENGTH_SHORT).show();  break;
+            case R.id.redo: Toast.makeText(this, "준비중입니다", Toast.LENGTH_SHORT).show();  break;
+            case R.id.save: Toast.makeText(this, "준비중입니다", Toast.LENGTH_SHORT).show();  break;
+        }
     }
 }
